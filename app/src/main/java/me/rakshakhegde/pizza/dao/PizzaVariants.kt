b2fa97a9ec69b853fa2fcc7893b3748380421d2a
Rakshak <rakshakhegde@gmail.com>
2017-09-20 18:44:38 +0530

    models setup

--- app/src/main/java/me/rakshakhegde/pizza/dao/PizzaVariants.kt
+++ app/src/main/java/me/rakshakhegde/pizza/dao/PizzaVariants.kt
@@ -3,4 +3,32 @@ package me.rakshakhegde.pizza.dao
 /**
  * Created by rakshak on 20/09/17.
  */
-class PizzaVariants
+
+
+data class PizzaVariants(
+		val variants: Variants
+)
+
+data class Variants(
+		val variant_groups: List<VariantGroup>,
+		val exclude_list: List<List<ExcludeList>>
+)
+
+data class ExcludeList(
+		val group_id: String, //1
+		val variation_id: String //3
+)
+
+data class VariantGroup(
+		val group_id: String, //1
+		val name: String, //Crust
+		val variations: List<Variation>
+)
+
+data class Variation(
+		val name: String, //Thin
+		val price: Int, //0
+		val default: Int, //1
+		val id: String, //1
+		val inStock: Int //1
+)
