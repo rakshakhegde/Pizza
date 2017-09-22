5c1d6a03d4c58998327fb0d5efd7b06d06b87048
Rakshak <rakshakhegde@gmail.com>
2017-09-22 20:34:52 +0530

    pizzaVariantsError

--- app/src/main/java/me/rakshakhegde/pizza/screen/main_screen/MainScreenViewModel.kt
+++ app/src/main/java/me/rakshakhegde/pizza/screen/main_screen/MainScreenViewModel.kt
@@ -1,5 +1,6 @@
 package me.rakshakhegde.pizza.screen.main_screen
 
+import android.databinding.ObservableBoolean
 import io.reactivex.schedulers.Schedulers
 import me.rakshakhegde.pizza.dao.PizzaApi
 import me.rakshakhegde.rxdatabinding.toField
@@ -10,8 +11,15 @@ import javax.inject.Inject
  */
 class MainScreenViewModel @Inject constructor(pizzaApi: PizzaApi) {
 
+	val pizzaVariantsLoading = ObservableBoolean(true)
+	val pizzaVariantsError = ObservableBoolean(false)
+
 	val pizzaVariants = pizzaApi.getPizzaVariants()
 			.subscribeOn(Schedulers.io())
+			.doOnError { throwable ->
+				pizzaVariantsError.set(true)
+			}
+			.doFinally { pizzaVariantsLoading.set(false) }
 			.toObservable()
 			.toField()
 }
